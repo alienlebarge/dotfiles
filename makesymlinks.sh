f9da3e8f91fd1703014715720a4ed5c9ca6483ea
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2014-08-28 11:39:41 +0200

    bower.io config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig npmrc"    # list of files/folders to symlink in homedir
+files="bowerrc gitconfig npmrc"    # list of files/folders to symlink in homedir
 
 ##########
 
