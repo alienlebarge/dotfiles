2d58a363917e0c66c1fe3a3d380fd8442a5ffe4d
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2014-12-17 14:23:22 +0100

    curl config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bowerrc gemrc gitconfig npmrc"    # list of files/folders to symlink in homedir
+files="bowerrc curlrc gemrc gitconfig npmrc"    # list of files/folders to symlink in homedir
 
 ##########
 
