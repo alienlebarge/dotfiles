20da9bb73d17506daf9ec21d9a47fed541ee151e
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2025-01-31 08:42:07 +0100

    remove Auto-completion and Key binding

--- .fzf.bash
+++ .fzf.bash
@@ -4,10 +4,4 @@ if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-source "/usr/local/opt/fzf/shell/completion.bash"
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+eval "$(fzf --bash)"
